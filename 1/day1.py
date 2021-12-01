6fc891c8e946765bcc0e5130acb63c97ba052a4b
Tom Wilent <tom.wilent@gmail.com>
2021-12-01 14:56:15 -0500

    did it

--- 1/day1.py
+++ 1/day1.py
@@ -3,7 +3,7 @@ import fileinput
 def ingest(research):
     depth = []
     for line in fileinput.input(files = research):
-        depth.append(line.rstrip())
+        depth.append(int(line.rstrip()))
     return depth
 
 def upordown(data):
@@ -23,6 +23,22 @@ def upordown(data):
     print("increased: ", increase)
     print("decreased: ", decrease)
 
+def windows(data):
+    last = ""
+    increase = 0
+    decrease = 0
+    for num in range(len(data)):
+        current = sum(data[num:num+3]) 
+        if last == "":
+            print(data[num], "(N/A - no previous measurement)")
+        elif current > last:
+            increase += 1
+            print(data[num], "(increased)")
+        elif current < last:
+            decrease += 1
+            print(data[num], "(decreased)")
+        last = current
+    print("increased: ", increase)
 
 if __name__ == "__main__":
-    upordown(ingest("1/input.txt"))
+    windows(ingest("1/input.txt"))
