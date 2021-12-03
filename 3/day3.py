a6b9ecc1612d845e19a26bb6702570dc510c6288
Tom Wilent <tom.wilent@gmail.com>
2021-12-03 12:52:16 -0500

    not there

--- 3/day3.py
+++ 3/day3.py
@@ -1,14 +1,28 @@
 import fileinput
 import pandas as pd
-
+import numpy as np
 
 def ingest(info):
     data = []
     for line in fileinput.input(files = info):
         data.append(list(line.rstrip()))
-    infodb = pd.DataFrame(data)
+    infodb = pd.DataFrame((data), 
+    columns=list('abcdefghijkl'))
     return infodb
 
+def co2(db, commondt):
+    col = 'abcdefghijkl'
+    count = 0
+    newdb = pd.Series([])
+    for i in commondt:
+        chk = col[count]
+        test = db.iat[0,count]
+        if newdb.empty:
+            newdb = db.query("{0} == {1}".format(chk, test))
+        else:
+            newdb = newdb.query("{0} == {1}".format(chk, test))
+        count += 1
+    return newdb     
 
 def onoff(x):
     if x == '1':
@@ -34,8 +48,10 @@ def binary2dec(binary):
 
 
 if __name__ == "__main__":
-    gamma, epsilon = findcommon(ingest("input.txt"))
+    raw = ingest("input.txt")
+    gamma, epsilon = findcommon(raw)
     g_int = binary2dec(gamma)
     e_int = binary2dec(epsilon)
+    print(co2(raw, gamma))
     power = g_int * e_int
     print(power)
